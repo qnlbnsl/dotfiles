8c9bf297181619c2726b99f4a252a51e1bf7b8bb
Kunal Bansal <46204848+qnlbnsl@users.noreply.github.com>
2023-04-10 16:38:15 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -208,10 +208,10 @@ golang_setup() {
 update_fs() {
   # Helps in general... Especially when coding in react
   # Increasing max watchers to 65535
-  $maxfiles = "fs.file-max = 65535"
+  maxfiles="fs.file-max = 65535"
   # Increasing max watchers. Each file watch consumes up to 1080 bytes.
   # 524288 will be able to use up to 540MB
-  $maxwatches = "fs.inotify.max_user_watches=524288"
+  maxwatches="fs.inotify.max_user_watches=524288"
   echo $maxfiles | sudo tee -a /etc/sysctl.conf
   echo $maxwatches | sudo tee -a /etc/sysctl.conf
 }
