cc1e6f00e76f2c5d89b4b6b09ff99324be2df99d
Kunal Bansal <kunal@immertec.com>
2024-05-14 19:42:22 +0000

    chore: Update user setup in Linux installer script and clone dotfiles

--- installers/linux/shell_functions/setup_user.sh
+++ installers/linux/shell_functions/setup_user.sh
@@ -24,4 +24,21 @@ create_new_user() {
   sudo usermod -aG sudo "$new_username"
   echo "$new_username ALL=(ALL) NOPASSWD:ALL" | sudo tee "/etc/sudoers.d/90-$new_username-root"
   echo "User $new_username created and added to sudo group."
+
+  echo "Updating ssh configuration..."
+  if [ ! -f ~/.ssh/authorized_keys ]; then
+    echo "No authorized_keys file found. Skipping."
+    return 0
+  fi
+  
+  sudo -u "$new_username" mkdir "/home/$new_username/.ssh"
+  cp ~/.ssh/authorized_keys "/home/$new_username/.ssh/authorized_keys"
+  sudo chown -R "$new_username:$new_username" "/home/$new_username/.ssh"
+  
+  echo "User created. Cloning dotfiles..."
+  git clone git@github.com:qnlbnsl/dotfiles.git "/home/$new_username/.dotfiles"
+  sudo chown -R "$new_username:$new_username" "/home/$new_username/.dotfiles"
+  echo "dotfiles cloned." 
+  echo "run: chmod +x /home/$new_username/.dotfiles/installer/linux/base.sh"
+  echo "run: /home/$new_username/.dotfiles/installer/linux/base.sh to setup the new user."
 }
